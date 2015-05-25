d28b915dd6d78e51cf5e0e2332eabb6dcc46b9e6
Maxim Salnikov <salnikov@gmail.com>
2015-05-26 00:24:40 +0200

    restangular+ui-grid added

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -9,24 +9,54 @@
  * Main module of the application.
  */
 angular
-  .module('openidmuiApp', [
+    .module('openidmuiApp', [
     'ngCookies',
     'ngMessages',
     'ngResource',
     'ngRoute',
-    'ngSanitize'
+    'ngSanitize',
+    'restangular',
+    'ui.grid'
   ])
-  .config(function ($routeProvider) {
-    $routeProvider
-      .when('/', {
-        templateUrl: 'views/main.html',
-        controller: 'MainCtrl'
-      })
-      .when('/about', {
-        templateUrl: 'views/about.html',
-        controller: 'AboutCtrl'
-      })
-      .otherwise({
-        redirectTo: '/'
-      });
-  });
+    .config(function ($routeProvider) {
+        $routeProvider
+            .when('/', {
+                templateUrl: 'views/main.html',
+                controller: 'MainCtrl'
+            })
+            .when('/users', {
+                templateUrl: 'views/about.html',
+                controller: 'AboutCtrl'
+            })
+            .otherwise({
+                redirectTo: '/'
+            });
+    })
+
+.config(function (RestangularProvider) {
+    RestangularProvider.setBaseUrl('/openidm/endpoint');
+
+    RestangularProvider.setDefaultHeaders({
+        'X-OpenIDM-Username': 'testuser',
+        'X-OpenIDM-Password': 'Password1'
+    });
+
+    RestangularProvider.setMethodOverriders(["put", "patch"]);
+
+    RestangularProvider.setRestangularFields({
+        id: "_id"
+    });
+
+    RestangularProvider.addResponseInterceptor(function (data, operation, what, url, response, deferred) {
+        var extractedData;
+        if (operation === "getList") {
+            extractedData = data.result[0].rows;
+        } else {
+            extractedData = data;
+        }
+        return extractedData;
+    });
+
+
+
+});
--- app/scripts/controllers/about.js
+++ app/scripts/controllers/about.js
@@ -8,10 +8,48 @@
  * Controller of the openidmuiApp
  */
 angular.module('openidmuiApp')
-  .controller('AboutCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  });
+
+.controller('AboutCtrl', ['$scope', 'Restangular',
+    function ($scope, Restangular) {
+
+        $scope.gridOptions = {
+            data: 'gridData',
+            enableRowSelection: true,
+            enableCellEditOnFocus: true,
+            showSelectionCheckbox: true,
+            showFooter: true,
+            columnDefs: [{
+                field: 'userName',
+                displayName: 'Username',
+                enableCellEdit: false
+            }, {
+                field: 'givenName',
+                displayName: 'First Name',
+                enableCellEdit: true
+            }, {
+                field: 'sn',
+                displayName: 'Last Name',
+                enableCellEdit: true
+            }, {
+                field: 'mail',
+                displayName: 'E-mail',
+                enableCellEdit: true
+            }, {
+                field: 'accountStatus',
+                displayName: 'Status',
+                enableCellEdit: true
+            }]
+        };
+
+        var userList = Restangular.all('jqgrid');
+
+        userList.getList({resource:'managed/user',_queryId:'get-managed-users',rows:'10',page:'1',sidx:'userName',sord:'asc'}).then(function (result) {
+            $scope.gridData = result.plain();
+        });
+
+        // Does a GET to /accounts
+        // Returns an empty array by default. Once a value is returned from the server
+        // that array is filled with those values. So you can use this in your template
+        // $scope.accounts = Restangular.all('accounts').getList().$object;
+
+    }]);
